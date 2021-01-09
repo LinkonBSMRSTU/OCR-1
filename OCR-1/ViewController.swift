6dee8adc4cb920067b344a9615709ae4eb7b14de
Fazle Rabbi Linkon <fazle.linkon@dreamonline.co.jp>
2021-01-09 12:27:45 +0600

    Initial Commit

---
+++ OCR-1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OCR-1
+//
+//  Created by Fazle Rabbi Linkon on 9/1/21.
+//  Copyright © 2021 Fazle Rabbi Linkon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
