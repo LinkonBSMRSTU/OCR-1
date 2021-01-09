77750f76395d8205bc90c393a61a0a8ed1ed39ee
Fazle Rabbi Linkon <fazle.linkon@dreamonline.co.jp>
2021-01-09 13:03:37 +0600

    - Gallery Image Data Read Complete

--- OCR-1/ViewController.swift
+++ OCR-1/ViewController.swift
@@ -10,11 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var galleryImageView: UIImageView!
+    @IBOutlet weak var galleryButton: UIBarButtonItem!
+    @IBOutlet weak var resultTextView: UITextView!
+    @IBOutlet weak var loadingActivity: UIActivityIndicatorView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        self.stopAnimating()
     }
 
+    private func startAnimating() {
+        self.loadingActivity.startAnimating()
+    }
+
+    private func stopAnimating() {
+        self.loadingActivity.stopAnimating()
+    }
+
+    @IBAction func galleryButtonTapped(_ sender: Any) {
+        setupGallery()
+    }
+
+    private func setupGallery() {
+
+        if(UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.photoLibrary)) {
+
+            let photoLibraryPicker = UIImagePickerController()
+            photoLibraryPicker.delegate = self
+            photoLibraryPicker.allowsEditing = true
+            photoLibraryPicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+            self.present(photoLibraryPicker, animated: true, completion: nil)
+
+        }
+
+    }
+
+
+}
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+
+        let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        self.galleryImageView.image = image
+    }
 
 }
 
